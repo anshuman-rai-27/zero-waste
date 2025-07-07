bd2678b530672c77956b7c483cd18dbc89ec7ce9
Rohitkumar367 <rohitddroid@gmail.com>
2025-07-07 21:55:09 +0530

    map done

---
+++ components/Map/Map.js
@@ -0,0 +1,48 @@
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer , Marker, Popup} from 'react-leaflet'
+import { locations } from '../../lib/location';
+import { Icon } from 'leaflet';
+import { useMemo } from 'react';
+
+const personIcon = new Icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/847/847969.png',
+  iconSize: [25, 25],
+});
+
+function getCentroid(coords) {
+  const latSum = coords.reduce((sum, loc) => sum + loc.lat, 0);
+  const lngSum = coords.reduce((sum, loc) => sum + loc.lng, 0);
+  return {
+    lat: latSum / coords.length,
+    lng: lngSum / coords.length,
+  };
+}
+
+function Map(){
+    const center = useMemo(()=>getCentroid(locations), []);
+
+    return(
+        <div className='w-full h-[500px] mt-10'>
+            <MapContainer center={[23.3441, 85.3096]} zoom={13} scrollWheelZoom={true}
+                className='w-full h-full'
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+
+                {locations.map((loc) => (
+                    <Marker key={loc.id} position={[loc.lat, loc.lng]} icon={personIcon}>
+                        <Popup>{loc.name}</Popup>
+                    </Marker>
+                ))}
+        
+                <Marker position={[center.lat, center.lng]}>
+                    <Popup>Suggested RVM Location (Center)</Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    )
+}
+
+export default Map;
